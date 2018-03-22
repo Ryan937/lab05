89c82966d888dc6c55099b7b9610a7c564ac6de1
Ryan937 <lsj937@gmail.com>
2018-03-22 16:21:32 -0700

    Finished creating XML data file

--- application/models/Tasks.php
+++ application/models/Tasks.php
@@ -7,34 +7,36 @@
  */
 
 class Tasks extends CSV_Model {
+    protected $CI;
 
     public function __construct()
     {
-            parent::__construct(APPPATH . '../data/tasks.csv', 'id');
+        parent::__construct(APPPATH . '../data/tasks.csv', 'id');
+        $this->CI = &get_instance();
     }
 
-	function getCategorizedTasks()
-	{
-		// extract the undone tasks
-		foreach ($this->all() as $task)
-		{
-			if ($task->status != 2)
-				$undone[] = $task;
-		}
+    function getCategorizedTasks()
+    {
+        // extract the undone tasks
+        foreach ($this->all() as $task)
+        {
+                if ($task->status != 2)
+                        $undone[] = $task;
+        }
 
-		// substitute the category name, for sorting
-		foreach ($undone as $task)
-			$task->group = $this->app->group($task->group);
+        // substitute the category name, for sorting
+        foreach ($undone as $task)
+                $task->group = $this->CI->app->group($task->group);
 
-		// order them by category
-		usort($undone, "orderByCategory");
+        // order them by category
+        usort($undone, "orderByCategory");
 
-		// convert the array of task objects into an array of associative objects
-		foreach ($undone as $task)
-			$converted[] = (array) $task;
+        // convert the array of task objects into an array of associative objects
+        foreach ($undone as $task)
+                $converted[] = (array) $task;
 
-	return $converted;
-	}
+        return $converted;
+    }
 
     // provide form validation rules
     public function rules()
